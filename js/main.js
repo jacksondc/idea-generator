104fa32816bb015cfa4f7804394cd35575109157
Jackson de Campos <jacksondecampos@gmail.com>
2015-01-19 13:28:45 -0800

    Update number on page load

--- js/main.js
+++ js/main.js
@@ -4,6 +4,12 @@ function updateIdea() {
   });
   $('#idea-old').clone().attr('id', 'idea').appendTo('#ideas').css({left: '-200%', right: '200%', opacity:'0'
     }).animate({ left: '0%', right: '0%', opacity:'1' });
+  updateIndex();
+}
+
+function updateIndex() {
   var index = Math.round( Math.pow(( Math.random() + 1 ),9) );
   $('#idea .index').text('#' + index);
 }
+
+updateIndex();
