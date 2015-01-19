4a76758237ba176126431d40006ad5c6eb353868
Jackson de Campos <jacksondecampos@gmail.com>
2015-01-19 13:19:56 -0800

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function updateIdea() {
+  $('#idea').attr('id', 'idea-old').animate({ left: '200%', right: '-200%', opacity:'0' }, function() {
+    this.remove();
+  });
+  $('#idea-old').clone().attr('id', 'idea').appendTo('#ideas').css({left: '-200%', right: '200%', opacity:'0'
+    }).animate({ left: '0%', right: '0%', opacity:'1' });
+  var index = Math.round( Math.pow(( Math.random() + 1 ),9) );
+  $('#idea .index').text('#' + index);
+}
