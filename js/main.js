f29bd44ce0b1eca29a8377fa8e267f1d61488596
Jackson de Campos <jacksondecampos@gmail.com>
2015-01-19 21:10:37 -0800

    Use JQuery remove instead of native

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 function updateIdea() {
   $('#idea').attr('id', 'idea-old').animate({ left: '200%', right: '-200%', opacity:'0' }, function() {
-    this.remove();
+    $(this).remove();
   });
   $('#idea-old').clone().attr('id', 'idea').appendTo('#ideas').css({left: '-200%', right: '200%', opacity:'0'
     }).animate({ left: '0%', right: '0%', opacity:'1' });
